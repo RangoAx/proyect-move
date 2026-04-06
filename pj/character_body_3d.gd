2bd1980b5c6f556ebed562bc38e025db7adffaf9
RangoAx <alanabichayn3@gmail.com>
2026-04-05 23:15:09 -0300

    cámara preliminar, y preview de salto

--- pj/character_body_3d.gd
+++ pj/character_body_3d.gd
@@ -1,1 +1,27 @@
 extends CharacterBody3D
+
+@onready var _camera := %Camera3D as Camera3D
+@onready var _camera_pivot := %CameraPivot as Node3D
+
+@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+@export var tilt_limit = deg_to_rad(75)
+
+@export var speed = 5.0
+@export var jump_velocity = 4
+@export var gravity: float = 5000
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	move_and_slide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_camera_pivot.rotation.x -= event.screen_relative.y * mouse_sensitivity
+		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
+		_camera_pivot.rotation.y += -event.screen_relative.x * mouse_sensitivity
