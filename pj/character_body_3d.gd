0d6fb3dd92bc0c024fb48ced8ee31e5878aed7e5
RangoAx <alanabichayn3@gmail.com>
2026-04-09 15:43:53 -0300

    movimiento básico

--- pj/character_body_3d.gd
+++ pj/character_body_3d.gd
@@ -16,6 +16,14 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
+	var input_direction := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction := (_camera_pivot.transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else: 
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	move_and_slide()
